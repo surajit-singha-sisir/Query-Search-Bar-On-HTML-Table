9c8ef702fe24db495bb7539611a11c3be7370e34
Surajit Singha Sisir <48810102+surajit-singha-sisir@users.noreply.github.com>
2023-05-24 12:53:45 +0600

    New style and script files added

--- script.js
+++ script.js
@@ -67,6 +67,8 @@ function sortTable(columnIndex) {
       case 1:
         return "dateSortIcon";
       case 2:
+        return "answerSortIcon";
+        case 3:
         return "descriptionSortIcon";
       default:
         return "";
@@ -94,7 +96,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //Search Bar Result
 function searchTable() {
-  var input = document.getElementById("searchInput").value.toLowerCase();
+  var input = document.getElementById("searchInput").value;
   var table = document.getElementById("dataTable");
   var rows = table.getElementsByTagName("tr");
 
@@ -103,13 +105,13 @@ function searchTable() {
     var rowText = "";
 
     for (var j = 0; j < rowData.length; j++) {
-      var cellText = rowData[j].innerText.toLowerCase();
+      var cellText = rowData[j].innerText;
       if (cellText.includes(input)) {
         var startIndex = cellText.indexOf(input);
         var endIndex = startIndex + input.length;
         var highlightedText = cellText.substring(startIndex, endIndex);
         rowData[j].innerHTML = cellText.replace(
-          highlightedText,
+          new RegExp(highlightedText, "g"),
           '<span class="highlight">' + highlightedText + '</span>'
         );
         rows[i].style.display = "";
@@ -117,9 +119,31 @@ function searchTable() {
         rowData[j].innerHTML = cellText;
         rows[i].style.display = "none";
       }
+
+      rowData[j].addEventListener("mouseenter", showQuerySuggestion);
+      rowData[j].addEventListener("mouseleave", hideQuerySuggestion);
     }
   }
 }
+
+
+function showQuerySuggestion(event) {
+  var cellText = event.target.innerText;
+  var query = encodeURIComponent(cellText);
+  var tooltip = document.createElement("div");
+  tooltip.classList.add("query-suggestion-tooltip");
+  tooltip.innerHTML = 'Search for "<a href="https://www.google.com/search?q=' + query + '" target="_blank">' + cellText + '</a>"';
+  event.target.appendChild(tooltip);
+}
+
+
+function hideQuerySuggestion(event) {
+  var tooltips = event.target.getElementsByClassName("query-suggestion-tooltip");
+  for (var i = 0; i < tooltips.length; i++) {
+    tooltips[i].remove();
+  }
+}
+
 
 
 // Night Mode Toggle
@@ -141,3 +165,87 @@ function toggleNightMode() {
     nightModeIcon.classList.add("fa-moon");
   }
 }
+
+//Retrieve DATA from JSON file
+var jsonData = []; // Global variable to store the JSON data
+var visibleRows = 20; // Number of rows initially visible
+var currentVisibleRows = visibleRows; // Counter for tracking the currently visible rows
+
+// Function to fetch JSON data from a file
+function fetchJSONData() {
+  var filename = "https://raw.githubusercontent.com/surajit-singha-sisir/Excel-File-to-JSON-Converter/master/phraseIdomsBanglaBCS.json";
+
+  fetch(filename)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      jsonData = data; // Store the fetched JSON data
+      populateTable(); // Call function to populate the table
+    })
+    .catch(function(error) {
+      console.log("Error fetching JSON data:", error);
+    });
+}
+
+// Function to populate the table with data
+function populateTable() {
+  var table = document.getElementById("dataTable");
+
+  for (var i = 0; i < jsonData.length && i < currentVisibleRows; i++) {
+    var row = table.insertRow(-1);
+    var idCell = row.insertCell(0);
+    var questionCell = row.insertCell(1);
+    var answerCell = row.insertCell(2);
+    var descriptionCell = row.insertCell(3);
+
+    idCell.innerHTML = jsonData[i].id;
+    questionCell.innerHTML = jsonData[i].question;
+    answerCell.innerHTML = jsonData[i].answer;
+    descriptionCell.innerHTML = jsonData[i].description;
+  }
+
+  if (jsonData.length > currentVisibleRows) {
+    // Show the "Show More" button if there are more rows to reveal
+    var showMoreButton = document.getElementsByClassName("show-more-button");
+    showMoreButton.addEventListener("click", function() {
+      revealMoreRows();
+      showMoreButton.style.display = "none"; // Hide the button after revealing more rows
+    });
+    table.parentNode.appendChild(showMoreButton);
+  }
+}
+
+// Function to reveal the next 20 rows
+function revealMoreRows() {
+  var table = document.getElementById("dataTable");
+  var remainingRows = jsonData.length - currentVisibleRows;
+
+  // Calculate the number of rows to reveal
+  var rowsToReveal = Math.min(visibleRows, remainingRows);
+  for (var i = currentVisibleRows; i < currentVisibleRows + rowsToReveal; i++) {
+    var row = table.insertRow(-1);
+    var idCell = row.insertCell(0);
+    var questionCell = row.insertCell(1);
+    var answerCell = row.insertCell(2);
+    var descriptionCell = row.insertCell(3);
+
+    idCell.innerHTML = jsonData[i].id;
+    questionCell.innerHTML = jsonData[i].question;
+    answerCell.innerHTML = jsonData[i].answer;
+    descriptionCell.innerHTML = jsonData[i].description;
+  }
+
+  currentVisibleRows += rowsToReveal;
+
+  if (currentVisibleRows >= jsonData.length) {
+    // Remove the "Show More" button if all rows have been revealed
+    var showMoreButton = table.parentNode.querySelector("button");
+    if (showMoreButton) {
+      showMoreButton.parentNode.removeChild(showMoreButton);
+    }
+  }
+}
+
+// Call the function to fetch JSON data and populate the initial rows
+fetchJSONData();
